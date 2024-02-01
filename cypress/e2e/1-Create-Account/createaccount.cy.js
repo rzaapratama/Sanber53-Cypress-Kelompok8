b9c74b23c4c22157524d8b4651ae8bd576da93c8
Putu Agus Mas Reza Pratama <macbook@192.168.1.8>
2024-02-01 23:45:16 +0800

    update script

--- cypress/e2e/1-Create-Account/createaccount.cy.js
+++ cypress/e2e/1-Create-Account/createaccount.cy.js
@@ -2,6 +2,8 @@ describe("Create an account", () => {
   it("Success create account", () => {
     cy.visit("https://magento.softwaretestingboard.com/");
     cy.get(".panel > .header > :nth-child(3) > a").click();
+    cy.get(".base").should("be.visible");
+    cy.get("p > a > img").should("be.visible");
     cy.get("#firstname").type("Reza");
     cy.get("#lastname").type("Pratama");
     cy.get("#email_address").type("putuagusmasreza.personal@gmail.com");
