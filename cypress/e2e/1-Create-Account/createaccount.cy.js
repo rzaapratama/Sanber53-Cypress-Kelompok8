415d9cb9d3870d73546bfd9973cd03e2921a3a43
Putu Agus Mas Reza Pratama <macbook@192.168.1.8>
2024-02-02 21:18:29 +0800

    update script

--- cypress/e2e/1-Create-Account/createaccount.cy.js
+++ cypress/e2e/1-Create-Account/createaccount.cy.js
@@ -1,6 +1,10 @@
 describe("Create an account", () => {
   it("Success create account", () => {
     cy.visit("https://magento.softwaretestingboard.com/");
+    cy.get(".content > p").should("have.text", "This is a demo store to test your test automaiton scripts. No orders will be fulfilled. If you are facing any issue, email us at hello@softwaretestingboard.com.");
+    cy.get(".panel.wrapper > .panel").should("be.visible");
+    cy.get("#search").should("be.visible");
+    cy.get(".logo > img").should("be.visible");
     cy.get(".panel > .header > :nth-child(3) > a").click();
     cy.get(".base").should("be.visible");
     cy.get("p > a > img").should("be.visible");
@@ -10,5 +14,7 @@ describe("Create an account", () => {
     cy.get("#password").type("Akubelajartesting123");
     cy.get("#password-confirmation").type("Akubelajartesting123");
     cy.get("#form-validate > .actions-toolbar > div.primary > .action").click();
+    cy.get(".message-error").should("be.visible");
+    cy.get("#form-validate > .actions-toolbar > div.primary > .action > span").should("have.text", "Create an Account");
   });
 });
